5bd6282bd6dda480deeb3a6709da540626e8a41b
Bartek Skoniecki <b.skoniecki@interia.pl>
2024-12-08 20:02:31 +0100

    :qw wq : i#

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
+    // Inkrementacja pola `age` za pomoc¹ metody `birthday`
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
 
-    // Twoja implementacja tutaj
-
+    // Wype³nienie wektora znakami 'y' lub 'n' w odwrotnej kolejnoœci
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) {
+        return person.isMonster() ? 'n' : 'y';
+    });
     return retval;
 }
